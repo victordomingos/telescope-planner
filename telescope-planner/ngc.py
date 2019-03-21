61d083eb267139da0c5ebc564f272fb326b3035a
victordomingos <editor.arcosonline@gmail.com>
2019-03-21 18:35:54 +0000

    ngc experiments

--- telescope-planner/ngc.py
+++ telescope-planner/ngc.py
@@ -2,9 +2,10 @@
 from pyongc import ongc
 from skyfield.api import load, Star, Topos
 
-from geocode import get_location
+from geocode import get_location, DEFAULT_LOCATION
 
-cur_place, _ = get_location()
+# cur_place, _ = get_location()
+cur_place = DEFAULT_LOCATION
 ts = load.timescale()
 t = ts.now()
 
@@ -12,48 +13,55 @@ planets = load('de421.bsp')
 earth = planets['earth']
 here = earth + Topos(f'{cur_place.latitude} N', f'{cur_place.longitude} E')
 
-star_names = ['NGC7000', 'NGC4548']
+star_names = ['NGC' + str(i) for i in range(1, 7000)]
 
 stars = []
 for name in star_names:
-    DSOobject = ongc.Dso(name)
-    ra_arr, dec_arr = DSOobject.getCoords()
-    ra, dec = tuple(ra_arr), tuple(dec_arr)
-    alt_ids = DSOobject.getIdentifiers()
-    messier = alt_ids[0]
-    constellation = DSOobject.getConstellation()
-    obj_type = DSOobject.getType()
-    #print('\nids:', alt_ids)
-    stars.append((name, ra, dec))
+    try:
+        DSOobject = ongc.Dso(name)
+        ra_arr, dec_arr = DSOobject.getCoords()
+        ra, dec = tuple(ra_arr), tuple(dec_arr)
+        alt_ids = DSOobject.getIdentifiers()
+        messier = alt_ids[0]
+        constellation = DSOobject.getConstellation()
+        obj_type = DSOobject.getType()
+        # print('\nids:', alt_ids)
+        stars.append((name, ra, dec))
+    except ValueError as e:
+        print(e)
 
-print('\n\nStars and other deep sky objects:')
-print('=================================')
+report = f'\n\n{len(stars)} currently visible stars and other deep sky objects:\n'
+print(report + len(report) * '=')
 
 out_of_sight_stars = []
 for name in star_names:
-    DSOobject = ongc.Dso(name)
-    ra_arr, dec_arr = DSOobject.getCoords()
-    ra, dec = tuple(ra_arr), tuple(dec_arr)
-    alt_ids = DSOobject.getIdentifiers()
-    messier = alt_ids[0]
-    if messier is None:
-        messier = 'N/A'
-    constellation = DSOobject.getConstellation()
-    obj_type = DSOobject.getType()
-    #print('\nids:', alt_ids)
+    try:
+        DSOobject = ongc.Dso(name)
+        ra_arr, dec_arr = DSOobject.getCoords()
+        ra, dec = tuple(ra_arr), tuple(dec_arr)
+        alt_ids = DSOobject.getIdentifiers()
+        messier = alt_ids[0]
+        if messier is None:
+            messier = 'N/A'
+        constellation = DSOobject.getConstellation()
+        obj_type = DSOobject.getType()
+        # print('\nids:', alt_ids)
 
-    star_obj = Star(ra_hours=ra, dec_degrees=dec)
-    star_astro = here.at(t).observe(star_obj)
-    star_app = star_astro.apparent()
-    alt, az, d = star_app.altaz('standard')
+        star_obj = Star(ra_hours=ra, dec_degrees=dec)
+        star_astro = here.at(t).observe(star_obj)
+        star_app = star_astro.apparent()
+        alt, az, d = star_app.altaz('standard')
 
-    if alt.degrees > 0.0:
-        print(f'\n{name.upper()} ({messier}) - {obj_type}:\n', '  ALT:', alt,
-              '\n   AZ:', az)
-        print('   Constellation:', constellation)
-    else:
-        out_of_sight_stars.append(name.upper())
+        if alt.degrees > 0.0:
+            print(f'\n{name.upper()} ({messier}) - {obj_type}:\n', '  ALT:', alt,
+                  '\n   AZ:', az)
+            print('   Constellation:', constellation)
+        else:
+            out_of_sight_stars.append(name.upper())
+    except ValueError as e:
+            print(e)
 
 if out_of_sight_stars:
-    print('\nThese are not visible:')
+    report2 = f'\n\n{len(out_of_sight_stars)} currently not visible:\n'
+    print(report2 + len(report) * '=')
     print(', '.join(out_of_sight_stars))
